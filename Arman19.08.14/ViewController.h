67370161b11cf0f39bb81e9568b9caa78e2532e6
itisioslab <itisioslab@Mac-mini-administrator-74.local>
2014-08-19 10:36:04 +0400

    Initial Commit

---
+++ Arman19.08.14/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Arman19.08.14
+//
+//  Created by itisioslab on 19.08.14.
+//  Copyright (c) 2014 ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Arman19.08.14Tests/Arman19_08_14Tests.m
@@ -0,0 +1,34 @@
+//
+//  Arman19_08_14Tests.m
+//  Arman19.08.14Tests
+//
+//  Created by itisioslab on 19.08.14.
+//  Copyright (c) 2014 ru. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Arman19_08_14Tests : XCTestCase
+
+@end
+
+@implementation Arman19_08_14Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
